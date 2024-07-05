2a652f1373b7963c63499e57a23dd9f31d92cd3f
pierobramanti <piero.bramanti94@gmail.com>
2024-07-05 16:36:12 +0200

    [UPD] add class to `square` with classList.add

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 // RECUPER L'ELEMENTO CHE CONTIENE LA MIA GRIGLIA//
-const grid = document.getElementById("grid");
+const grid = document.getElementById(`grid`);
 
 // APRO CICLO FOR//
-for( let i = 0; i< 0 ; i++ ){
-
+for( let i = 0; i< 100 ; i++ ){
+// CREO IL SINGOLO QUADRATO//
     let currentSquare = document.createElement("div");
+//ASSEGNAZIONE DELLA CLASSE SQUARE ALL'ELEMENTO CREATO
+    currentSquare.classList.add(`square`)
 
     currentSquare.innerText = i+ 1;
 
+// APPENDO IL QUADRATO NELL'ELEMTO DIV CREATO PRECEDENTEMENTE
     grid.append(currentSquare);
 }
