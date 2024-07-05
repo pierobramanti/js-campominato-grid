0763c9a689a32770f9508dd8801ae6e5ba373428
pierobramanti <piero.bramanti94@gmail.com>
2024-07-05 17:27:46 +0200

    [UPD] moved function indside event click of button

--- js/script.js
+++ js/script.js
@@ -14,19 +14,24 @@ const btn= document.getElementById(`play`)
 // RECUPER L'ELEMENTO CHE CONTIENE LA MIA GRIGLIA//
 const grid = document.getElementById(`grid`);
 
+// ASSEGNO L' EVENTO CLICK AL PULSANTE//
+btn.addEventListener(`click`, function(){
 // APRO CICLO FOR//
 for( let i = 0; i< 100 ; i++ ){
-// CREO IL SINGOLO QUADRATO//
-    let currentSquare = createSquare();
+    // CREO IL SINGOLO QUADRATO//
+        let currentSquare = createSquare();
+    
+        currentSquare.innerText = i+ 1;
+    
+        currentSquare.addEventListener(`click`, function(){
+            console.log([i+1])
+    
+            this.classList.toggle("clicked");
+        });
+    
+    // APPENDO IL QUADRATO NELL'ELEMTO DIV CREATO PRECEDENTEMENTE
+        grid.append(currentSquare);
+    }
 
-    currentSquare.innerText = i+ 1;
+})
 
-    currentSquare.addEventListener(`click`, function(){
-        console.log([i+1])
-
-        this.classList.toggle("clicked");
-    });
-
-// APPENDO IL QUADRATO NELL'ELEMTO DIV CREATO PRECEDENTEMENTE
-    grid.append(currentSquare);
-}
