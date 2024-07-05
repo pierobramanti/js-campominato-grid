f4b6bf14f2b0cb3d00d7b7e10e0baff02f8f5520
pierobramanti <piero.bramanti94@gmail.com>
2024-07-05 16:50:46 +0200

    [UPD] defined function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+// DEFINIZIONE DELLA FUNZIONE CHE MI CREA I QUADRATI//
+
+function createSquare(){
+    let currentElement = document.createElement("div");
+//ASSEGNAZIONE DELLA CLASSE SQUARE ALL'ELEMENTO CREATO
+    currentElement.classList.add(`square`)
+
+// RESTITUISCO IL QUADRATO NELLA VARIABILE CURRENT ELEMENT //
+    return currentElement;
+}
+
+
 // RECUPER L'ELEMENTO CHE CONTIENE LA MIA GRIGLIA//
 const grid = document.getElementById(`grid`);
 
 // APRO CICLO FOR//
 for( let i = 0; i< 100 ; i++ ){
 // CREO IL SINGOLO QUADRATO//
-    let currentSquare = document.createElement("div");
-//ASSEGNAZIONE DELLA CLASSE SQUARE ALL'ELEMENTO CREATO
-    currentSquare.classList.add(`square`)
+    let currentSquare = createSquare();
 
     currentSquare.innerText = i+ 1;
 
